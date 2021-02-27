7c3fdbdcc7dce4538dcb4b0acc8dc05b5144fc96
Kolby Law <kolbytlaw@gmail.com>
2021-02-26 17:26:43 -0700

    Added initial API routes for Goals. Updated api/index.js to include goal-routes.js.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
+const goalRoutes = require('./goal-routes.js');
 
 router.use('/users', userRoutes);
+router.use('/goals', goalRoutes);
 
 module.exports = router;
