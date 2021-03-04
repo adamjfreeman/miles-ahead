3b0d3aabda8ab4efc94038506e0772d37739ae89
Kenny <kenneththelarson@gmail.com>
2021-03-03 19:39:41 -0700

    goals updated

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -17,10 +17,6 @@ router.get('/login', (req, res) => {
 });
 
 router.get('/goals', withAuth, (req, res) => {
-    if (req.session.loggedIn) {
-        res.redirect('/');
-        return;
-    }
 
     res.render('goals');
 });
